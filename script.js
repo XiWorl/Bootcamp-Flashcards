35065516f1ad0ed7f0aa9f8ecc132516ba289045
XiWorl <BlackWrlds@gmail.com>
2024-10-23 00:01:50 -0400

    Added clicking functionality

--- script.js
+++ script.js
@@ -7,20 +7,32 @@ const flashcards = [
 // You can use flashcards.length to get the length of the array
 
 // These two variables will come in handy
+let card = document.getElementById("flashcard")
 let currentIndex = 0;
 let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
     if (showingTerm == true) {
-    let card = document.getElementById("flashcard")
-    card.innerText = flashcards[0].term
+        let card = document.getElementById("flashcard")
+        card.innerText = flashcards[currentIndex].term
+    }
+    else {
+        let card = document.getElementById("flashcard")
+        card.innerText = flashcards[currentIndex].definition
+        // showingTerm = false
     }
 }
 
 displayCard()
 
 // The rest of the code you will write is apart of event listeners
+card.addEventListener("click", () => (
+    showingTerm = (showingTerm == true) ? false : true,
+    // console.log("Hello world!"+showingTerm)
+    displayCard()
+    // console.log(showingTerm + "He")
+))
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
